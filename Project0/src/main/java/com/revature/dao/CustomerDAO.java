617a2d5f6a92945d8ad844bf1720f35bf4d5062b
Roberth <roberhtorreshomar@gmail.com>
2020-04-28 18:02:08 -0400

    new classes

--- Project0/src/main/java/com/revature/dao/CustomerDAO.java
+++ Project0/src/main/java/com/revature/dao/CustomerDAO.java
@@ -1,11 +1,28 @@
 package com.revature.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.revature.beans.Customer;
+import com.revature.beans.Employee;
 
 public interface CustomerDAO {
 	
 	public void createCustomer();
 	public List<Object> getCustomers();
+	
+public static List<Customer> customerList = new ArrayList<Customer>();
+	
+	//to find a customer by first name
+	public static Customer findCustomerByName(String inputName) {
+		for(int i = 0; i < customerList.size(); i++) {
+			String name = customerList.get(i).getFirstName();
+			if(inputName.equals(name)) {
+				return customerList.get(i);
+			}
+		}
+		System.out.println("Customer not found");
+		
+		return null;
+	}
 }
---
+++ Project0/src/main/java/com/revature/menu/Menu.java
@@ -0,0 +1,9 @@
+package com.revature.menu;
+
+import java.util.Scanner;
+
+public class Menu {
+		
+		static Scanner scan = new Scanner(System.in);
+
+}
