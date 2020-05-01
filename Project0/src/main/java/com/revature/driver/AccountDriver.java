e229d28affcfd933d8350aa9471ee1a58d47157c
jpark1914 <parkerj4321@gmail.com>
2020-05-01 16:47:35 -0500

    Finalized project

--- Project0/src/main/java/com/revature/driver/AccountDriver.java
+++ Project0/src/main/java/com/revature/driver/AccountDriver.java
@@ -7,8 +7,10 @@ import com.revature.dao.AccountDAO;
 import com.revature.dao.AccountDAOImpl;
 import com.revature.dao.CustomerDAO;
 import com.revature.dao.CustomerDAOImpl;
+import com.revature.dao.EmployeeDAOImpl;
 import com.revature.io.AccountsIO;
 import com.revature.io.CustomerIO;
+import com.revature.io.EmployeeIO;
 import com.revature.menu.Menu;
 
 public class AccountDriver {
@@ -19,6 +21,8 @@ public class AccountDriver {
 	public static void main(String[] args) {
 		CustomerIO.readCustomerFile();
 		AccountsIO.readAccountFile();
+		EmployeeIO.readEmployeeFile();
+//		System.out.println(EmployeeDAOImpl.employeeList);
 
 		Menu.startMenu();
 		
